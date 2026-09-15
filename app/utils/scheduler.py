23fb5348ca0e890bbef5595c891d89ecc3afc5f7
ivancernikov <chernikof50@gmail.com>
2026-09-15 18:21:20 +0300

    a

--- app/utils/scheduler.py
+++ app/utils/scheduler.py
@@ -7,8 +7,8 @@ def setup_scheduler(start_reminders, process_reminders, bot):
     scheduler.add_job(
         start_reminders,
         trigger="cron",
-        hour=9,  # 9
-        minute=0,
+        hour=15,  # 9
+        minute=23,
         args=[bot]
     )
 
